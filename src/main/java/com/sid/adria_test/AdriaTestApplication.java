c09ea3d110bd3a264e3dfc1f46d5ac3240b2be68
NAJAH <mohamednajah2424@gmail.com>
2023-10-30 09:25:16 +0100

    wallet-service

---
+++ src/main/java/com/sid/adria_test/AdriaTestApplication.java
@@ -0,0 +1,13 @@
+package com.sid.adria_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdriaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdriaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sid/adria_test/AdriaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.adria_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdriaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
